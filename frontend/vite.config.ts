92a0fa3a991e8d7be64810449c1f880b1339ed8e
Siddheshwar Gupta <siddheshwargupta081@gmail.com>
2026-07-06 16:35:23 +0545

    Configure Github Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Hack4Impact/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
